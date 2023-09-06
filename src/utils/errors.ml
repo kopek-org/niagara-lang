7d11d79996047a2a11229db7c4fa700ae86ca475
Fabrice Le Fessant <fabrice.le_fessant@ocamlpro.com>
2023-09-06 11:25:36 +0200

    Add AGPL headers to all source files

--- src/utils/errors.ml
+++ src/utils/errors.ml
@@ -1,3 +1,13 @@
+(*****************************************************************************)
+(*                                                                           *)
+(*  Copyright (c) 2023 OCamlPro SAS                                          *)
+(*                                                                           *)
+(* All rights reserved.                                                      *)
+(* This source code is licensed under the GNU Affero General Public License  *)
+(* version 3 found in the LICENSE.md file in the root directory of this      *)
+(* source tree.                                                              *)
+(*                                                                           *)
+(*****************************************************************************)
 
 let raise_error ?(with_pos=Pos.dummy) ?span fmt =
   Format.kfprintf
--- src/utils/pos.mli
+++ src/utils/pos.mli
@@ -1,4 +1,13 @@
-
+(*****************************************************************************)
+(*                                                                           *)
+(*  Copyright (c) 2023 OCamlPro SAS                                          *)
+(*                                                                           *)
+(* All rights reserved.                                                      *)
+(* This source code is licensed under the GNU Affero General Public License  *)
+(* version 3 found in the LICENSE.md file in the root directory of this      *)
+(* source tree.                                                              *)
+(*                                                                           *)
+(*****************************************************************************)
 
 (** This module gives positions related oprations.  
     
