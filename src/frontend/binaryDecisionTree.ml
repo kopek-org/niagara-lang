5b5e4725831841d61f029cb63023619f2a8ae511
Fabrice Le Fessant <fabrice.le_fessant@ocamlpro.com>
2023-09-06 11:25:36 +0200

    Add AGPL headers to all source files

--- src/frontend/binaryDecisionTree.ml
+++ src/frontend/binaryDecisionTree.ml
@@ -1,3 +1,13 @@
+(*****************************************************************************)
+(*                                                                           *)
+(*  Copyright (c) 2023 OCamlPro SAS                                          *)
+(*                                                                           *)
+(* All rights reserved.                                                      *)
+(* This source code is licensed under the GNU Affero General Public License  *)
+(* version 3 found in the LICENSE.md file in the root directory of this      *)
+(* source tree.                                                              *)
+(*                                                                           *)
+(*****************************************************************************)
 
 module Make(Cond : Map.OrderedType)(KnowledgeMap : Map.S with type key = Cond.t) = struct
 
