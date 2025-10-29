20b63108d246d2ffe3d404eadec9c7e8691189f9
Pierre Boutillier <pierre.boutillier@laposte.net>
2025-10-29 13:59:58 +0100

    Encodings for ValueType

--- src/commons/valueType.ml
+++ src/commons/valueType.ml
@@ -1,7 +1,12 @@
-type t =
-  | TInteger
-  | TRational
-  | TMoney
-  | TEvent
-  | TDate
-  | TDuration
+type t = TInteger | TRational | TMoney | TEvent | TDate | TDuration
+
+let encoding =
+  Json_encoding.string_enum
+    [
+      ("int", TInteger);
+      ("rational", TRational);
+      ("money", TMoney);
+      ("event", TEvent);
+      ("date", TDate);
+      ("duration", TDuration);
+    ]
