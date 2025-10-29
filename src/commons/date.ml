bd56313202c073982f82e6451a94fa3083466c7c
Pierre Boutillier <pierre.boutillier@laposte.net>
2025-10-29 13:54:49 +0100

    Encodings for Date

--- src/commons/date.ml
+++ src/commons/date.ml
@@ -1,5 +1,23 @@
 open CalendarLib
 
-module Date = Date
+module Date = struct
+  include Date
 
-module Duration = Date.Period
+  let encoding =
+    let open Json_encoding in
+    conv
+      (fun d -> (year d, int_of_month (month d), day_of_month d))
+      (fun (y, m, d) -> make y m d)
+      (obj3 (req "year" int) (req "month" int) (req "day" int))
+end
+
+module Duration = struct
+  include Date.Period
+
+  let encoding =
+    let open Json_encoding in
+    conv
+      (fun d -> ymd d)
+      (fun (y, m, d) -> make y m d)
+      (obj3 (req "year" int) (req "month" int) (req "day" int))
+end
